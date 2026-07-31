eed2919152783cf01155adaf8b05a335e8e1fbea
Gabriel Teixeira Machado <126582816+MachadONLY@users.noreply.github.com>
2026-07-31 19:30:10 -0300

    Remove club moment card from career home

---
+++ src/home-v2-cleanup.js
@@ -0,0 +1,13 @@
+import "./home-v2-cleanup.css";
+
+function removeClubMoment() {
+  document.querySelector(".career-v2 .v2-moment")?.remove();
+}
+
+removeClubMoment();
+
+const app = document.querySelector("#app");
+if (app) {
+  const observer = new MutationObserver(removeClubMoment);
+  observer.observe(app, { childList: true, subtree: true });
+}
