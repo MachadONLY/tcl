7148070c69d6903b105cdea93d1c56a25feada83
Gabriel Teixeira Machado <126582816+MachadONLY@users.noreply.github.com>
2026-08-05 10:06:09 -0300

    test: use Youri Tielemans' verified FotMob player id

--- tests/official-football-data-smoke.mjs
+++ tests/official-football-data-smoke.mjs
@@ -11,7 +11,7 @@ const fotmobHtml = `
     <tr><td><a href="/players/15903/michael-carrick">Michael Carrick</a></td><td>Coach</td><td>England</td><td></td><td>45</td><td></td><td></td></tr>
     <tr><td><a href="/players/866967/altay-bayindir">Altay Bayindir</a></td><td>GK</td><td>Türkiye</td><td>1</td><td>28</td><td></td><td>€5.8M</td></tr>
     <tr><td><a href="/players/273018/andrey-santos">Andrey Santos</a></td><td>DM, CM</td><td>Brazil</td><td>17</td><td>22</td><td></td><td>€44.4M</td></tr>
-    <tr><td><a href="/players/203574/youri-tielemans">Youri Tielemans</a></td><td>DM, AM, CM</td><td>Belgium</td><td>18</td><td>29</td><td></td><td>€31.7M</td></tr>
+    <tr><td><a href="/players/465960/youri-tielemans">Youri Tielemans</a></td><td>DM, AM, CM</td><td>Belgium</td><td>18</td><td>29</td><td></td><td>€35.2M</td></tr>
     <tr><td><a href="/players/422685/bruno-fernandes">Bruno Fernandes</a></td><td>AM, DM</td><td>Portugal</td><td>8</td><td>31</td><td></td><td>€33.6M</td></tr>
     <tr><td><a href="/players/1293027/ethan-wheatley">Ethan Wheatley</a></td><td>ST, AM</td><td>England</td><td>36</td><td>20</td><td></td><td>€155.5K</td></tr>
   </tbody>
