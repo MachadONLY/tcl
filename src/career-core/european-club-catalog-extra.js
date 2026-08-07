c9ca3aa6abfc48f8802b2adb9827af5814589f92
Gabriel Teixeira Machado <126582816+MachadONLY@users.noreply.github.com>
2026-08-06 22:28:25 -0300

    Expand friendly browser across the rest of Europe

---
+++ src/career-core/european-club-catalog-extra.js
@@ -0,0 +1,132 @@
+export const EXTRA_EUROPE_COUNTRIES = Object.freeze([
+  ['ROU','Romênia','#f0d462',[
+    ['Liga I',1,['FCSB','CFR Cluj','Universitatea Craiova','Rapid București','Dinamo București','Farul Constanța','Universitatea Cluj','Sepsi OSK','Petrolul Ploiești','UTA Arad','Oțelul Galați','Hermannstadt','Botoșani','Unirea Slobozia','Argeș Pitești','Csikszereda']],
+    ['Liga II',2,['Steaua București','Corvinul Hunedoara','Voluntari','Concordia Chiajna','Politehnica Iași','Ceahlăul','Metaloglobus','CSM Reșița','FCU Craiova','Chindia Târgoviște','Bihor Oradea','Afumați']]
+  ]],
+  ['BUL','Bulgária','#77b784',[
+    ['First League',1,['Ludogorets','Levski Sofia','CSKA Sofia','Botev Plovdiv','Lokomotiv Plovdiv','Cherno More','Arda Kardzhali','Slavia Sofia','Beroe','Lokomotiv Sofia','Septemvri Sofia','Spartak Varna','Botev Vratsa','Hebar','Dobrudzha','Montana']],
+    ['Second League',2,['Etar','Pirin Blagoevgrad','Marek Dupnitsa','Minyor Pernik','Belasitsa Petrich','Yantra Gabrovo','Dunav Ruse','Strumska Slava','Lovech','Fratria','Sportist Svoge','Nesebar']]
+  ]],
+  ['HUN','Hungria','#d97979',[
+    ['NB I',1,['Ferencváros','Puskás Akadémia','Fehérvár','Paks','Debrecen','Újpest','Diósgyőr','MTK Budapest','Zalaegerszeg','Kecskemét','Nyíregyháza','Kisvárda']],
+    ['NB II',2,['Vasas','Honvéd','Gyirmót','Szeged-Csanád','Soroksár','Budafok','Ajka','Csákvár','Mezőkövesd','Kozármisleny','BVSC-Zugló','Tatabánya']]
+  ]],
+  ['SVK','Eslováquia','#6e9bd6',[
+    ['Niké Liga',1,['Slovan Bratislava','Spartak Trnava','DAC Dunajská Streda','Žilina','Ružomberok','Trenčín','Podbrezová','Dukla Banská Bystrica','Skalica','Košice','Komárno','Tatran Prešov']],
+    ['2. Liga',2,['Zlaté Moravce','Petržalka','Púchov','Pohronie','Liptovský Mikuláš','Humenné','Malženice','Šamorín','Považská Bystrica','Zvolen','Stará Ľubovňa','Slovan Bratislava B']]
+  ]],
+  ['SVN','Eslovênia','#73a6df',[
+    ['PrvaLiga',1,['Olimpija Ljubljana','Maribor','Celje','Koper','Mura','Domžale','Bravo','Radomlje','Primorje','Aluminij']],
+    ['2. SNL',2,['Gorica','Triglav Kranj','Nafta 1903','Beltinci','Krka','Dravinja','Bilje','Tabor Sežana','Rudar Velenje','Ilirija 1911']]
+  ]],
+  ['BIH','Bósnia e Herzegovina','#5f94cf',[
+    ['Premier League',1,['Zrinjski Mostar','Borac Banja Luka','Željezničar','Sarajevo','Velež Mostar','Široki Brijeg','Sloga Doboj','Posušje','Radnik Bijeljina','Igman Konjic','Sloboda Tuzla','Rudar Prijedor']],
+    ['First League',2,['Čelik Zenica','Sloboda Tuzla','Stupčanica Olovo','GOŠK Gabela','Travnik','Jedinstvo Bihać','Gradina Srebrenik','Zvijezda Gradačac','Laktaši','Kozara Gradiška']]
+  ]],
+  ['ALB','Albânia','#df5c5c',[
+    ['Kategoria Superiore',1,['Egnatia','Partizani Tirana','Tirana','Vllaznia','Teuta Durrës','Laçi','Dinamo City','Skënderbeu','Bylis','Flamurtari']],
+    ['Kategoria e Parë',2,['Elbasani','Apolonia Fier','Besa Kavajë','Korabi Peshkopi','Kastrioti','Lushnja','Burreli','Tomori Berat','Kukësi','Pogradeci']]
+  ]],
+  ['MKD','Macedônia do Norte','#dc6e6e',[
+    ['First League',1,['Shkëndija','Vardar','Shkupi','Struga','Sileks','Rabotnički','Tikveš','Gostivar','Pelister','Besa Dobërdoll','Brera Strumica','Makedonija GP']],
+    ['Second League',2,['Belasica','Bregalnica Štip','Detonit Plachkovica','Kozhuf','Ohrid','Pobeda','Sasa','Skopje','Vardarski','Novaci']]
+  ]],
+  ['MNE','Montenegro','#d86a6a',[
+    ['First League',1,['Budućnost Podgorica','Sutjeska Nikšić','Dečić','Mornar Bar','Petrovac','Arsenal Tivat','Jezero','Jedinstvo Bijelo Polje','Otrant-Olympic','Bokelj']],
+    ['Second League',2,['Rudar Pljevlja','Lovćen','Kom Podgorica','Grbalj','Igalo','Iskra Danilovgrad','Podgorica','Internacional Podgorica']]
+  ]],
+  ['CYP','Chipre','#d7c964',[
+    ['First Division',1,['APOEL','Omonia','Pafos','Aris Limassol','AEK Larnaca','Apollon Limassol','Anorthosis','AEL Limassol','Ethnikos Achna','Nea Salamis','Karmiotissa','Omonia Aradippou','Enosis Neon Paralimni','Akritas Chlorakas']],
+    ['Second Division',2,['Olympiakos Nicosia','Doxa Katokopias','ASIL Lysi','Ayia Napa','Digenis Morphou','Peyia 2014','PAEEK','MEAP Nisou','Spartakos Kitiou','Halkanoras Idaliou']]
+  ]],
+  ['IRL','Irlanda','#61b47a',[
+    ['Premier Division',1,['Shamrock Rovers','Bohemians','Derry City','St Patrick’s Athletic','Shelbourne','Sligo Rovers','Dundalk','Drogheda United','Galway United','Waterford']],
+    ['First Division',2,['Cork City','Finn Harps','Cobh Ramblers','Treaty United','Athlone Town','Bray Wanderers','Wexford','UCD','Kerry FC','Longford Town']]
+  ]],
+  ['NIR','Irlanda do Norte','#65ad84',[
+    ['Premiership',1,['Linfield','Larne','Glentoran','Cliftonville','Crusaders','Coleraine','Ballymena United','Glenavon','Dungannon Swifts','Carrick Rangers','Portadown','Loughgall']],
+    ['Championship',2,['Bangor','Newry City','Institute','Annagh United','Ards','Dundela','H&W Welders','Ballinamallard United','Limavady United','Newington','Armagh City','Ballyclare Comrades']]
+  ]],
+  ['WAL','País de Gales','#d75f5f',[
+    ['Cymru Premier',1,['The New Saints','Connah’s Quay Nomads','Bala Town','Newtown','Caernarfon Town','Cardiff Metropolitan','Haverfordwest County','Penybont','Barry Town United','Flint Town United','Briton Ferry Llansawel','Aberystwyth Town']],
+    ['Cymru North/South',2,['Airbus UK Broughton','Colwyn Bay','Holywell Town','Mold Alexandra','Buckley Town','Llandudno','Llanelli Town','Pontypridd United','Cambrian United','Trethomas Bluebirds','Carmarthen Town','Trefelin BGC']]
+  ]],
+  ['FIN','Finlândia','#74a7d9',[
+    ['Veikkausliiga',1,['HJK Helsinki','KuPS','Ilves','SJK','Inter Turku','VPS','Haka','Mariehamn','Oulu','Gnistan','Jaro','KTP']],
+    ['Ykkösliiga',2,['TPS','Lahti','JIPPO','SalPa','EIF','PK-35','JäPS','KäPa','SJK Akatemia','Klubi 04']]
+  ]],
+  ['ISL','Islândia','#78a9d8',[
+    ['Besta deild',1,['Víkingur Reykjavík','Valur','Breiðablik','KR Reykjavík','FH','Stjarnan','Fram','KA Akureyri','ÍA','Vestri','Afturelding','Leiknir Reykjavík']],
+    ['1. deild',2,['Keflavík','Grindavík','Fjölnir','Þróttur Reykjavík','ÍBV','Njarðvík','Thor Akureyri','Dalvík/Reynir','Selfoss','HK Kópavogur']]
+  ]],
+  ['LVA','Letônia','#b46f71',[
+    ['Virslīga',1,['RFS','Riga FC','Valmiera','Auda','Liepāja','Daugavpils','Jelgava','Tukums 2000','Metta','Grobiņa']],
+    ['1. līga',2,['Super Nova','JDFS Alberts','Skanste','Leevon PPK','Riga FC II','RFS II','Ogre United','Smiltene','Mārupe','Rēzekne']]
+  ]],
+  ['LTU','Lituânia','#d7c96e',[
+    ['A Lyga',1,['Žalgiris Vilnius','Kauno Žalgiris','Hegelmann','Panevėžys','Sūduva','Banga','Džiugas','Šiauliai','TransINVEST','Riteriai']],
+    ['I Lyga',2,['Be1 NFA','Neptūnas','Babrungas','Minija','Atmosfera','Tauras','Garliava','Ekranas','Jonava','Žalgiris B']]
+  ]],
+  ['EST','Estônia','#75a6d8',[
+    ['Meistriliiga',1,['Flora Tallinn','Levadia Tallinn','Nõmme Kalju','Paide Linnameeskond','Narva Trans','Tammeka','Kuressaare','Vaprus','Harju JK Laagri','Tallinna Kalev']],
+    ['Esiliiga',2,['Viimsi','Elva','Tabasalu','Tallinna Legion','Flora U21','Levadia U21','Nõmme United','FC Tallinn','Tartu Welco','Paide U21']]
+  ]],
+  ['GEO','Geórgia','#d16c6c',[
+    ['Erovnuli Liga',1,['Dinamo Tbilisi','Dinamo Batumi','Iberia 1999','Torpedo Kutaisi','Dila Gori','Samgurali','Telavi','Gagra','Kolkheti Poti','Gareji']],
+    ['Erovnuli Liga 2',2,['Sioni Bolnisi','Rustavi','Spaeri','Lokomotivi Tbilisi','WIT Georgia','Merani Martvili','Aragvi Dusheti','Dinamo Tbilisi II','Gonio','Meshakhte']]
+  ]],
+  ['ARM','Armênia','#d97a65',[
+    ['Premier League',1,['Pyunik','Ararat-Armenia','Noah','Urartu','Alashkert','Ararat Yerevan','Shirak','Van','West Armenia','Gandzasar']],
+    ['First League',2,['Lernayin Artsakh','BKMA II','Syunik','Andranik','Mika','Ararat-Armenia II','Pyunik II','Urartu II','Noah II','Shirak II']]
+  ]],
+  ['AZE','Azerbaijão','#69a7bf',[
+    ['Premier League',1,['Qarabağ','Neftçi','Sabah','Zira','Sumgayit','Gabala','Turan Tovuz','Araz-Naxçıvan','Kapaz','Shamakhi']],
+    ['First Division',2,['Karvan','MOIK Baku','Mingachevir','Difai Ağsu','Baku Sporting','Jabrayil','Qaradağ Lökbatan','Zaqatala','Imishli','Energetik Mingachevir']]
+  ]],
+  ['MDA','Moldávia','#6aa4d8',[
+    ['Super Liga',1,['Sheriff Tiraspol','Petrocub Hîncești','Zimbru Chișinău','Milsami Orhei','Dacia Buiucani','Spartanii Sportul','Florești','Bălți']],
+    ['Liga 1',2,['Victoria Chișinău','Saxan','Fălești','Iskra Rîbnița','Ungheni','Real Succes','Speranis Nisporeni','Olimp Comrat']]
+  ]],
+  ['KOS','Kosovo','#6e9fda',[
+    ['Superleague',1,['Ballkani','Drita','Prishtina','Gjilani','Llapi','Malisheva','Dukagjini','Ferizaj','Suhareka','Feronikeli 74']],
+    ['First League',2,['Vushtrria','Trepça','Liria Prizren','Besa Pejë','Flamurtari Prishtina','Vëllaznimi','Ramiz Sadiku','Dinamo Ferizaj','Rilindja 1974','2 Korriku']]
+  ]],
+  ['MLT','Malta','#d36c6c',[
+    ['Premier League',1,['Floriana','Hamrun Spartans','Hibernians','Birkirkara','Sliema Wanderers','Valletta','Mosta','Balzan','Gżira United','Marsaxlokk','Naxxar Lions','Żabbar St. Patrick']],
+    ['Challenge League',2,['Sirens','Santa Lucia','Pietà Hotspurs','Tarxien Rainbows','Marsa','Swieqi United','Zurrieq','Fgura United','Lija Athletic','St Andrews']]
+  ]],
+  ['LUX','Luxemburgo','#75a6d8',[
+    ['National Division',1,['F91 Dudelange','Swift Hesperange','Differdange 03','Progrès Niederkorn','Racing Luxembourg','Jeunesse Esch','Mondorf-les-Bains','UNA Strassen','Victoria Rosport','Wiltz 71','Hostert','Rodange 91','Mondercange','Bettembourg']],
+    ['Division of Honour',2,['Rumelange','Avenir Beggen','Berdenia Berbourg','Etzella Ettelbruck','Mamer 32','Canach','Koeppchen Wormeldange','Marisca Mersch','Schifflange 95','Alisontia Steinsel']]
+  ]],
+  ['FRO','Ilhas Faroé','#74a6d9',[
+    ['Premier League',1,['KÍ Klaksvík','HB Tórshavn','B36 Tórshavn','Víkingur Gøta','NSÍ Runavík','EB/Streymur','07 Vestur','B68 Toftir','Skála','ÍF Fuglafjørður']],
+    ['1. deild',2,['TB Tvøroyri','AB Argir','B71 Sandoy','FC Hoyvík','KÍ II','HB II','B36 II','NSÍ II','Víkingur II','Suduroy']]
+  ]],
+  ['KAZ','Cazaquistão','#63a8c1',[
+    ['Premier League',1,['Astana','Kairat','Aktobe','Ordabasy','Tobol','Kyzylzhar','Atyrau','Zhetysu','Kaisar','Elimai','Turan','Okzhetpes','Ulytau','Zhenis']],
+    ['First Division',2,['Shakhter Karagandy','Taraz','Ekibastuz','Akademiya Ontustik','Khan Tengri','Altai','Arys','Akzhayik','Kaspiy','SD Family','Jas Qyran','Zhetisay']]
+  ]],
+  ['ISR','Israel','#78a7d7',[
+    ['Premier League',1,['Maccabi Tel Aviv','Maccabi Haifa','Hapoel Be’er Sheva','Beitar Jerusalem','Maccabi Netanya','Hapoel Haifa','Bnei Sakhnin','Ashdod','Hapoel Jerusalem','Maccabi Petah Tikva','Ironi Tiberias','Hapoel Petah Tikva','Hapoel Tel Aviv','Kiryat Shmona']],
+    ['Liga Leumit',2,['Bnei Yehuda','Hapoel Kfar Saba','Hapoel Ramat Gan','Hapoel Acre','Hapoel Rishon LeZion','Maccabi Herzliya','Hapoel Nof HaGalil','Hapoel Afula','Kafr Qasim','Hapoel Ra’anana']]
+  ]],
+  ['AND','Andorra','#77a8d8',[
+    ['Primera Divisió',1,['Inter Club d’Escaldes','FC Santa Coloma','UE Santa Coloma','Atlètic Club d’Escaldes','Penya Encarnada','Ordino','Pas de la Casa','Esperança d’Andorra']],
+    ['Segona Divisió',2,['La Massana','Rangers','Encamp','Carroi','Atlètic Amèrica','Santa Coloma B']]
+  ]],
+  ['SMR','San Marino','#73a5d8',[
+    ['Campionato Sammarinese',1,['Tre Penne','La Fiorita','Folgore','Tre Fiori','Cosmos','Virtus','Libertas','Murata','Domagnano','Juvenes/Dogana','Faetano','Fiorentino','San Giovanni','Cailungo','Pennarossa','Academy San Marino']]
+  ]],
+  ['GIB','Gibraltar','#d76d6d',[
+    ['National League',1,['Lincoln Red Imps','Europa FC','St Joseph’s','Bruno’s Magpies','Lynx','Mons Calpe','Manchester 62','Glacis United','College 1975','Europa Point','Lions Gibraltar']]
+  ]],
+  ['BLR','Belarus','#d36d6d',[
+    ['Premier League',1,['Dinamo Minsk','BATE Borisov','Neman Grodno','Torpedo-BelAZ','Dinamo Brest','Shakhtyor Soligorsk','Slavia Mozyr','Gomel','Isloch Minsk','Vitebsk','Minsk','Smorgon','Naftan','Arsenal Dzerzhinsk','Dnepr Mogilev','Molodechno']],
+    ['First League',2,['Belshina Bobruisk','Lida','Baranovichi','Orsha','Volna Pinsk','Bumprom','Ostrovets','Lokomotiv Gomel','Niva Dolbizno','ABFF U19','Dinamo Minsk II','BATE II']]
+  ]],
+  ['RUS','Rússia','#d56b6b',[
+    ['Premier League',1,['Zenit Saint Petersburg','CSKA Moscow','Spartak Moscow','Dynamo Moscow','Lokomotiv Moscow','Krasnodar','Rostov','Rubin Kazan','Akhmat Grozny','Krylia Sovetov','Fakel Voronezh','Orenburg','Pari Nizhny Novgorod','Khimki','Baltika Kaliningrad','Sochi']],
+    ['First League',2,['Torpedo Moscow','Ural','Arsenal Tula','Chayka','Rotor Volgograd','Chernomorets Novorossiysk','SKA-Khabarovsk','Yenisey','Rodina Moscow','Neftekhimik','Sokol Saratov','Shinnik Yaroslavl','KAMAZ','Ufa','Tyumen','Alania Vladikavkaz']]
+  ]]
+]);
